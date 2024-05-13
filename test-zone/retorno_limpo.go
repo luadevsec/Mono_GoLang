7c812fa63615db84630de7d4bb401565c98de3f6
Lunna Cipher <lunna.devsec@gmail.com>
2024-05-13 07:33:25 -0300

    Create retorno_limpo.go

---
+++ test-zone/retorno_limpo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func soma(a, b int) int {
+	return a + b
+}
+
+
+
+
+
+
+func subtracao(a, b int) (result int) {
+	result = a - b
+	return 
+}
+
+
+func main() {
+	fmt.Println("Olá, mundo!")
+	fmt.Println(soma(1, 2))
+	fmt.Println(subtracao(1, 2))
+}
